0722da7f9d841f750c5860489ece299e49ae80ea
sunnywizy <sundayobasi222@gmail.com>
2024-08-07 17:44:41 +0100

    I added some javascript files to my repository

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,16 @@
-export default function guardrail(mathFunction) {
-    const queue = [];
-    let result;
-    try {
-      result = mathFunction();
-    } catch (error) {
-      result = `Error: ${error.message}`;
-    }
-    queue.push(result);
-    queue.push('Guardrail was processed');
-  
-    return queue;
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+  try {
+    // upload a photo asynchronously
+    photo = await uploadPhoto();
+    // create user asynchronously
+    user = await createUser();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  return { photo, user };
 }
