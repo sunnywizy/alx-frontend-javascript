c45adc6234f9fb87415e977cb29b06b8498d85e3
sunnywizy <sundayobasi222@gmail.com>
2024-08-07 17:15:41 +0100

    I added some javascript files to my repository

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -7,5 +7,4 @@ export default function divideFunction(numerator, denominator) {
     } catch (error) {
       throw Error('cannot divide by 0');
     }
-  }
-  
+}
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -10,5 +10,4 @@ export default function guardrail(mathFunction) {
     queue.push('Guardrail was processed');
   
     return queue;
-  }
-  
+}
